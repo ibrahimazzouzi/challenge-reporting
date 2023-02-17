a9aba283c8b0688fecd71a6831fc68263db11c27
ibrahimazzouzi <ibrahimazzouzi.contact@gmail.com>
2023-02-17 15:29:29 +0100

    fix : saving grades to the database is a lot faster now

--- fetch-db.js
+++ fetch-db.js
@@ -3,75 +3,64 @@ const request = require('request')
 const knex = require('./db.js')
 
 const gradesFilePath = './grades.json'
+const dbFilePath = './students.db';
 
-console.log('Fetching DB....')
-request('https://outlier-coding-test-data.onrender.com/students.db')
-  .pipe(fs.createWriteStream('students.db'))
-  .on('finish', onDbFetched)
+(async function () {
+  console.log('Fetching DB....')
+  request('https://outlier-coding-test-data.onrender.com/students.db')
+    .pipe(fs.createWriteStream(dbFilePath))
+    .on('error', e => handleError(e, 'Error saving db file'))
+    .on('finish', fetchGrades)
+}())
 
-function onDbFetched () {
-  console.log('db was fetched succesfully.')
-  let grades = readGradesFile()
-  if (grades) return populateGrades(grades)
-  console.log('Fetching grades.json from source')
+function fetchGrades () {
+  console.log('Fetching grades...')
   request('https://outlier-coding-test-data.onrender.com/grades.json')
-    .pipe(fs.createWriteStream('grades.json'))
-    .on('finish', () => {
-      grades = readGradesFile()
-      if (grades) return populateGrades(grades)
-      console.log('Could not read grades file : ', gradesFilePath)
-    })
+    .pipe(fs.createWriteStream(gradesFilePath))
+    .on('error', e => handleError(e, 'Error saving grades.json'))
+    .on('finish', addGrades)
 }
 
-function readGradesFile () {
-  let grades = null
-  if (fs.existsSync(gradesFilePath)) {
-    try {
-      grades = JSON.parse(fs.readFileSync(gradesFilePath))
-      return grades
-    } catch (e) {
-      console.log('error reading from file system or grades file is not a valid JSON file')
-      knex.destroy()
-    }
-  }
-  return null
-}
-
-const populateGrades = (grades) => {
+async function addGrades () {
   console.log('Adding grades table...')
-  createTable('grades').then(async _ => {
-    console.log('grades table added.')
-    console.log('adding records...')
-    let added = 0
-    for (const record of grades) {
-      try {
-        await addRecord(record)
-      } catch (e) {
-        console.error('Error adding records to db : ', e)
-        knex.destroy()
-      }
-      added += 1
+  try {
+    await createTable('grades')
+    const grades = JSON.parse(fs.readFileSync(gradesFilePath, 'utf8'))
+    const chunks = []
+    const chunkSize = 500
+    for (let i = 0; i < grades.length; i += chunkSize) {
+      chunks.push(grades.slice(i, i + chunkSize))
     }
-    console.log(
-     `Added ${added} records | remote file has : ${grades.length} records
-    `)
+    await addRecords(chunks, chunkSize)
+    console.log('finished adding grades table')
     knex.destroy()
-  })
+  } catch (e) {
+    handleError(e, 'error adding grades data to db')
+  }
 }
 
-const createTable = tableName => (
-  knex.schema.createTable(tableName, table => {
+function createTable (tableName) {
+  return knex.schema.createTable(tableName, table => {
     table.increments()
     table.integer('student_id')
     table.string('course')
     table.integer('grade')
   })
-)
+}
 
-const addRecord = student => (
-  knex('grades').insert({
-    student_id: student.id,
-    course: student.course,
-    grade: student.grade
-  })
-)
+async function addRecords (chunks, chunkSize) {
+  for (let chunk of chunks) {
+    chunk = chunk.map(gradeRecord => ({
+      student_id: gradeRecord.id,
+      course: gradeRecord.course,
+      grade: gradeRecord.grade
+    }))
+    await knex.batchInsert('grades', chunk, chunkSize)
+  }
+}
+
+function handleError (error, errorStr) {
+  console.log('error message : ', errorStr)
+  console.log(error)
+  knex.destroy()
+}
